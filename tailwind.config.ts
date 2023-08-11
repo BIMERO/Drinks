8d955fae9b03bcd91f0108ac0b0f970d1809efae
Rejoice Olugbenga <rejoice.olugbenga@stu.cu.edu.ng>
2023-08-11 23:26:28 +0100

    correction

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./content/**/*.{js,ts,jsx,tsx,mdx}",
-    "./layout/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Content/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Layout/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
